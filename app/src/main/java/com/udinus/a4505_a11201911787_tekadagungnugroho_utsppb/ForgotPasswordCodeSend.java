048a34e2f6308c656e70cb6eecfaae36e7d59d75
Tekad Agung Nugroho <83072925+OxidiLily@users.noreply.github.com>
2021-11-02 05:41:43 +0700

    revisi

--- app/src/main/java/com/udinus/a4505_a11201911787_tekadagungnugroho_utsppb/ForgotPasswordCodeSend.java
+++ app/src/main/java/com/udinus/a4505_a11201911787_tekadagungnugroho_utsppb/ForgotPasswordCodeSend.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class ForgotPasswordCodeSend extends AppCompatActivity {
+    EditText editTextCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_code_send);
+
+        editTextCode = findViewById(R.id.edt_txt_code);
     }
 
     public void clickLSendCode(View view) {
+        if (TextUtils.isEmpty(editTextCode.getText().toString().trim())){
+            editTextCode.setError(getString(R.string.email_pass_nama_Kosong));
+        }else{
         Intent i = new Intent(ForgotPasswordCodeSend.this, LoginEmail.class);
         startActivity(i);
         Toast.makeText(view.getContext(),"Reset Password Berhasil",Toast.LENGTH_LONG).show();
+        }
     }
 }
